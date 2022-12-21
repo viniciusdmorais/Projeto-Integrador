2592a01fe5432a951a1c9a87f1a28bdff3f0b9b2
Vinicius <viny0012@gmail.com>
2022-12-21 16:07:25 -0300

    Ajustes na index.html e criação index.css, ajuste no slider tempo menor

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,13 +1,12 @@
-
-var sliders  = document.querySelectorAll('#sliders li');  /*Pega todos os elementos li da lista não ordenada	retornando um objeto NodeList, 	de acordo com posicionamento encontrado
+let sliders  = document.querySelectorAll('#sliders li');  /*Pega todos os elementos li da lista não ordenada	retornando um objeto NodeList, 	de acordo com posicionamento encontrado
 no documento*/
-var current  = 0;                                       /* Define o indice, que será responsavel  por informar os slider atual */
-var total    = sliders.length - 1;                      /* Retorna o total de itens do slide. Diminui menos 1, devido o primeiro item começa com zero.  Se temos 2 items, sua posição:
+let current  = 0;                                       /* Define o indice, que será responsavel  por informar os slider atual */
+let total    = sliders.length - 1;                      /* Retorna o total de itens do slide. Diminui menos 1, devido o primeiro item começa com zero.  Se temos 2 items, sua posição:
 0,1,2*/
 
 window.setInterval(function() {                         /*  Executaremos uma função num intervalo de tempo.  E defiremos que executará em 2000 milesegundos*/
-	var index =  current ? current - 1 : total;         /*	 a variavel index será responsável pela	 posição do elemento que iremos remover a classe	*/
+	let index =  current ? current - 1 : total;         /*	 a variavel index será responsável pela	 posição do elemento que iremos remover a classe	*/
 	sliders[index].className  = '';                     /*Pega o elemento pra remover a classe*/	
 	sliders[current].className  = 'slider-active';      /*Adiciona a classe no elemento atual*/
 	current = current >= total ? 0 : current+1; 	    /*Calcular a posição do próximo elemento que será exibido*/
-}, 7000);
+}, 4000);
