df37e4496e8e2e0ee0666acebe84b52f49f04d89
Vinicius <viny0012@gmail.com>
2023-01-07 20:41:41 -0300

    colocando a descrição na frente da imagem

--- assets/js/top_receitas_index.js
+++ assets/js/top_receitas_index.js
@@ -249,10 +249,12 @@ function CriaTopReceitas() {
     const elemRank = document.createElement("article"); // variavel sendo criada com o novo elemento tag article
     // abaixo estou incluindo dentro do article um innerHTML com tags populadas pelo objeto rankReceitas
     elemRank.innerHTML = `
+        <figure id="nome_receitas">
             <a href="receitas_prontas_tst.html?receitaID=${rankReceitas[i].idReceita}">
                 <img src="${rankReceitas[i].imagem}" alt="${rankReceitas[i].nomeReceita}">
+                <figcaption> ${rankReceitas[i].nomeReceita} </figcaption>
             </a>
-              
+         <figure>     
             `;
 
     secAntes.appendChild(elemRank); // agora estou appendando o novo elemento
